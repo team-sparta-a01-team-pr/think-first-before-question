7925b86cfe28768fb1951666b577870db36da878
hunzz <hunzz.study@gmail.com>
2024-01-09 17:33:49 +0900

    질문 도메인 초기 세팅

--- src/main/kotlin/com/sparta/tfbq/domain/question/controller/QuestionController.kt
+++ src/main/kotlin/com/sparta/tfbq/domain/question/controller/QuestionController.kt
@@ -1,9 +1,10 @@
 package com.sparta.tfbq.domain.question.controller
 
+import com.sparta.tfbq.domain.question.service.QuestionService
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1/questions")
-class QuestionController {
+class QuestionController(val service: QuestionService) {
 }
