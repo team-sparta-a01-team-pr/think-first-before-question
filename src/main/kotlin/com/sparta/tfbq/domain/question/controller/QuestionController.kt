4224119ea59df5bfcb1b471328626f406d05f252
hunzz <hunzz.study@gmail.com>
2024-01-09 20:41:01 +0900

    Feat: 질문 등록 기능 구현
    - QuestionController에 addQuestion 메서드 생성
    - 질문 등록 시 요구되는 요청 DTO 생성(AddQuestionRequest)
    - QuestionService에 addQuestion 메서드 생성
    - 튜터는 질문을 등록할 수 없도록 예외처리 (WrongRoleException)

--- src/main/kotlin/com/sparta/tfbq/domain/question/controller/QuestionController.kt
+++ src/main/kotlin/com/sparta/tfbq/domain/question/controller/QuestionController.kt
@@ -1,10 +1,14 @@
 package com.sparta.tfbq.domain.question.controller
 
+import com.sparta.tfbq.domain.question.dto.request.AddQuestionRequest
 import com.sparta.tfbq.domain.question.service.QuestionService
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/questions")
 class QuestionController(private val service: QuestionService) {
+    @PostMapping("/{memberId}")
+    fun addQuestion(@RequestBody request: AddQuestionRequest, @PathVariable memberId: Long) {
+        service.addQuestion(memberId, request)
+    }
 }
--- src/main/kotlin/com/sparta/tfbq/domain/question/service/QuestionService.kt
+++ src/main/kotlin/com/sparta/tfbq/domain/question/service/QuestionService.kt
@@ -1,9 +1,28 @@
 package com.sparta.tfbq.domain.question.service
 
 import com.sparta.tfbq.domain.member.repository.MemberRepository
+import com.sparta.tfbq.domain.question.dto.request.AddQuestionRequest
+import com.sparta.tfbq.domain.question.model.Question
 import com.sparta.tfbq.domain.question.repository.QuestionRepository
+import com.sparta.tfbq.global.exception.ModelNotFoundException
+import com.sparta.tfbq.global.exception.WrongRoleException
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
-class QuestionService(private val questionRepository: QuestionRepository, private val memberRepository: MemberRepository) {
+class QuestionService(
+    private val questionRepository: QuestionRepository,
+    private val memberRepository: MemberRepository
+) {
+    fun addQuestion(memberId: Long, request: AddQuestionRequest) {
+        val member = memberRepository.findByIdOrNull(memberId)
+            ?: throw ModelNotFoundException(memberId, "Member")
+
+        // 튜터는 질문을 할 수 없다
+        if (member.role.toString() == "TUTOR") throw WrongRoleException(member.role.toString())
+
+        val question = Question(member, request.tutorId, request.title, request.content, request.isPrivate)
+        member.questions.add(question)
+        memberRepository.save(member)
+    }
 }
