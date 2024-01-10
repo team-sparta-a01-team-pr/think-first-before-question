667c1367226b8fcddcb0f798c8202de741e065c1
hunzz <hunzz.study@gmail.com>
2024-01-10 23:56:12 +0900

    Feat: 질문 수정 기능 구현 (3차 수정)
    - data class를 class로 변경

--- src/main/kotlin/com/sparta/tfbq/global/exception/ModelNotFoundException.kt
+++ src/main/kotlin/com/sparta/tfbq/global/exception/ModelNotFoundException.kt
@@ -1,4 +1,4 @@
 package com.sparta.tfbq.global.exception
 
-data class ModelNotFoundException(val modelName: String) :
+class ModelNotFoundException(val modelName: String) :
     RuntimeException("Model '$modelName' not found with given id.")
--- src/main/kotlin/com/sparta/tfbq/global/exception/WrongRoleException.kt
+++ src/main/kotlin/com/sparta/tfbq/global/exception/WrongRoleException.kt
@@ -1,4 +1,4 @@
 package com.sparta.tfbq.global.exception
 
-data class WrongRoleException (val role: String)
+class WrongRoleException (val role: String)
     : RuntimeException("${role}s do not have permission to write.")
