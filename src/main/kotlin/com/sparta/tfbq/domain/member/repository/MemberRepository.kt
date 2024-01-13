65b6503b1f02f25c67846425d1537f1d66ebc0d5
SEUNGHAN BAEK <84169773+back-seung@users.noreply.github.com>
2024-01-13 17:21:29 +0900

    Remove/#31 Member Entity 잔재 메서드 삭제 (#32)
    * dev <- feat/#25 질문 도메인 관련 클래스 코드 리팩토링 (#26)
    
    Refactor: QuestionService 코드 리팩토링 (1차)
    
    - QuestionService 내 반복되는 코드를 내부 메서드로 분리
    - QuestionController에 deleteQuestion 메서드의 mapping URI 수정 (/{questionId}/{memberId})
    - QuesitonController 내 들여쓰기 조정
    - UpdateQuestionRequest에서 tutorId 속성 제거
    - QuestionService의 updateQuestion에서 tutorID 유효성 검사 로직 삭제
    - 관련 테스트 코드 수정
    
    * refactor: 멤버 엔티티 분리 작업
    
    - Controller, Service 분리
    - 기존 MemberController, MemberService에는 공통로직만 남겨 놓음
    - MemberResponse 상속 관계 해제 및 Response 재분리
    - 도메인 분리로 인해 WrongRoleException 메시지 작성하는 방식으로 수정
    
    * refactor: 질문 & 답변 엔티티 리팩토링
    
    - Question(N) -> Member(1) 편의 메서드 작성
    - Answer(N) -> Question(1) 편의 메서드 작성
      - 관련된 서비스 코드 수정
    - 패키지 구조 변경으로 인한
    - QuestionService 업데이트 메서드 수정
    
    * refactor: 멤버 엔티티 분리 수정
    
    - StudentService 닉네임 유무 확인하는 책임을 StudentCreateRequest로 위임
    - RandomNicknameGenerator object로 변경
    
    * User Entity 잔재 메서드 삭제
    
    ---------
    
    Co-authored-by: hunzzzzz <152062846+hunzzzzz@users.noreply.github.com>

--- src/main/kotlin/com/sparta/tfbq/domain/member/repository/MemberRepository.kt
+++ src/main/kotlin/com/sparta/tfbq/domain/member/repository/MemberRepository.kt
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository
 interface MemberRepository : JpaRepository<Member, Long> {
     fun existsByEmail(email: String): Boolean
     fun existsByNickname(nickname: String): Boolean
+    fun existsEmailAndPassword(name: String, password: String): Boolean
 }
